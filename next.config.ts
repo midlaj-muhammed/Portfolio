b54be8f14ab87a0b8cdb45841974095df981a87a
Muhammed Midlaj <midlajvalappil@gmail.com>
2026-02-08 23:09:28 +0530

    fix: update next config with assetPrefix for gh-pages images

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
+const repoName = "Portfolio";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/Portfolio" : "",
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   images: {
     unoptimized: true,
   },
